9d66a06d192c413b5e24708150bd306c84baf403
maks <mamonov.maksim.20003@gmail.com>
2022-09-21 18:46:37 +0500

    создан класс car, ему присвоены св-ва и в нём создан конструктор

--- src/Сar.java
+++ src/Сar.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Сar {
+public class Сar {
+    String brand;
+    String model;
+    float engineVolume;
+    String color;
+    int productionYear;
+    String productionCountry;
+
+    void Car(String brand, String model, float engineVolume,
+        String color, int productionYear, String productionCountry) {
+        this.brand = brand;
+        this.model = model;
+        this.engineVolume = engineVolume;
+        this.color = color;
+        this.productionYear = productionYear;
+        this.productionCountry = productionCountry;
+    }
+
 }
