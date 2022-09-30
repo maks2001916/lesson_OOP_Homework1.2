be587e4d705aba225d961170ecfbc630f96cd08e
maks <mamonov.maksim.20003@gmail.com>
2022-09-30 20:25:19 +0500

    первое задание треьтьего д.з. урока инкарсуляция доработано

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,40 @@
+import Transport.Car;
+
 public class Main {
     public static void main(String[] args)
     {
         Car Lada = new Car("Lada", null, 1.7,
-                "желтый", 2015, "Россия");
+                "желтый", 2015, "Россия",
+                "автомат", null, "a111aa111",
+                5, "зимняя");
         Car Audi = new Car("Audi", "A8 50 L TDI quattro", 3.0,
-                "чёрный", 2020, "Германия");
+                "чёрный", 2020, "Германия",
+                "автомат", null, "b222bb222",
+                5, "летняя");
         Car BMW = new Car("BMW", "Z8", 3.0,
-                "чёрный", 2021, "Германия");
+                "чёрный", 2021, "Германия",
+                "автомат", null, "c333cc333",
+                5, "зимняя");
         Car Kia = new Car("Kia", "Sportage 4 поколениие", 2.4,
-                "красный", 2018, "Южная Корея");
+                "красный", 2018, "Южная Корея",
+                "механика", null, "639629",
+                5, "летняя");
         Car Hyungai = new Car("Hyundai", "Avante", 1.6,
-                "оранжевый", 2016, "Южная Корея");
+                "оранжевый", 2016, "Южная Корея",
+                "механика", null, "e555ee555",
+                5, "зимняя");
         Lada.Car();
         Audi.Car();
         BMW.Car();
         Kia.Car();
         Hyungai.Car();
+
+        Lada.setInsurance = "5463728394";
+        Audi.insurance = "9173849234";
+        BMW.insurance = "759510628";
+        Kia.insurance = "680193853";
+        Hyungai.insurance = "384719473";
+
+        Lada.
     }
 }
